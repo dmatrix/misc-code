59b9f3454b89bb8e44a7501f2222000bf26f2929
Jules Damji <dmatrix@comcast.net>
2020-02-26 06:23:54 -0800

    tests for testing OSS model registery API

---
+++ py/mlflow/log_reg_model.py
@@ -0,0 +1,67 @@
+import os
+import shutil
+import pprint
+from pprint import pprint as pp
+
+from random import random, randint
+import mlflow.sklearn
+from mlflow import log_metric, log_param, log_artifacts
+from sklearn.ensemble import RandomForestRegressor
+from mlflow.tracking import MlflowClient
+import warnings
+
+if __name__ == "__main__":
+
+    warnings.filterwarnings("ignore")
+    print(mlflow.__version__)
+
+    # set the tracking server to be Databricks Community Edition
+    # set the experiment name; if name does not exist, MLflow will
+    # create one for you
+    local_registry = "sqlite:///mlruns_db"
+    print(f"Running local model registry={local_registry}")
+    model_name = "sk-learn-random-forest-reg-model"
+    mlflow.set_tracking_uri(local_registry)
+    with mlflow.start_run(run_name="LOCAL_REGISTRY") as run:
+        params = {"n_estimators": 3, "random_state": 0}
+        sk_learn_rfr = RandomForestRegressor(params)
+
+        # Log params using the MLflow sklearn APIs
+        mlflow.log_params(params)
+        log_param("param_1", randint(0, 100))
+        log_metric("metric_1", random())
+        log_metric("metric_2", random() + 1)
+        log_metric("metric_33", random() + 2)
+        # Log and register the model at the same time
+        mlflow.sklearn.log_model(
+                    sk_model = sk_learn_rfr,
+                    artifact_path = "sklearn-model",
+                    registered_model_name="sk-learn-random-forest-reg-model")
+        if not os.path.exists("outputs"):
+         os.makedirs("outputs")
+        with open("outputs/test.txt", "w") as f:
+            f.write("Looks, like I logged to the local store!")
+
+        log_artifacts("outputs")
+        shutil.rmtree('outputs')
+        run_id = run.info.run_uuid
+    #
+    # register with model registry
+    # on a local host
+    #
+    client = MlflowClient()
+    result = mlflow.register_model(
+                "runs:/3f57ae604b6a4b51a10b72d6e5a13b63/artifacts/sklearn-model",
+                model_name)
+    print(result)
+
+    result = client.create_model_version(
+            name="sk-learn-random-forest-reg-model",
+            source="mlruns/0/3f57ae604b6a4b51a10b72d6e5a13b63/artifacts/sk-model",
+            run_id="3f57ae604b6a4b51a10b72d6e5a13b63")
+    print(result)
+    [pp(dict(rm), indent=4) for rm in client.list_registered_models()]
+    # Get a list of specific versions of the named models
+    print(f"List of Model = {model_name} and Versions")
+    print("=" * 80)
+    [pp(dict(mv), indent=4) for mv in client.search_model_versions("name='sk-learn-random-forest-reg-model'")]
