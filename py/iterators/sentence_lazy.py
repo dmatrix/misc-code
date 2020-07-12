b51371bd13fb92693ef644907cfebb418613cc78
Jules Damji <dmatrix@comcast.net>
2020-07-12 15:11:01 -0700

    added generators expresssions example for Fluent Python
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

---
+++ py/iterators/sentence_lazy.py
@@ -0,0 +1,24 @@
+import re
+import reprlib
+from typing import Pattern
+
+RE_WORD: Pattern[str] = re.compile("\w+")
+
+class LazySentence:
+   def __init__(self, text):
+      # No need to have a words list
+      self.text = text
+
+   def __repr__(self):
+      return 'LazySentence(%s' % reprlib.repr(self.text)
+
+   def __iter__(self):
+      # build an iterator over matches on RE_WORD on self.text, yielding MatchObject instance
+      for match in RE_WORD.finditer(self.text):
+         yield match.group()
+
+if __name__ == '__main__':
+   s = LazySentence('"The time has come," the Walrus said,')
+   print(s)
+   for w in s:
+      print(w)
