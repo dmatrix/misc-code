6fcdfe7057f67a96a8216039dde4ab4b8377e94a
Jules Damji <dmatrix@comcast.net>
2020-07-21 10:59:51 -0700

    added a new issue file
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

---
+++ py/mlflow/mlflow_issues/issue_3114.py
@@ -0,0 +1,23 @@
+import joblib
+import mlflow
+from mlflow.tracking import MlflowClient
+
+if __name__ == '__main__':
+   with open('test.file', 'wb') as f:
+      test = range(20)
+      joblib.dump(test, f)
+   mlflow.set_tracking_uri("http://127.0.0.1:5000")
+   exp_info = MlflowClient().get_experiment_by_name("issue_3114")
+   if exp_info is None:
+      exp_id = MlflowClient().create_experiment("issue_3114")
+   else:
+      exp_id = exp_info.experiment_id
+   with mlflow.start_run(experiment_id=exp_id) as run:
+      run_id = run.run_id = run.info.run_uuid
+
+      params = {"n_estimators": 3, "random_state": 0}
+      mlflow.log_params(params)
+      mlflow.log_artifact('test.file')
+
+      print(f"artifact_uri={mlflow.get_artifact_uri()}")
+      print(f"run_id={run_id}")
