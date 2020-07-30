a33bfb9f6bafe189a1191c6a5bc42285340e0a11
Jules Damji <dmatrix@comcast.net>
2020-07-29 20:38:02 -0700

    added more APIs examples snippets
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

--- py/mlflow/model_registry/search_registered_models.py
+++ py/mlflow/model_registry/search_registered_models.py
@@ -12,17 +12,12 @@ if __name__ == "__main__":
     # set the tracking server to be localhost with sqlite as tracking store
     local_registry = "sqlite:///mlruns.db"
     print(f"Running local model registry={local_registry}")
-    model_name="WeatherForecastModel"
+    model_name="IrisClassifierModel"
     mlflow.set_tracking_uri(local_registry)
 
     # Search model versions
     print(f"List of all versions of {model_name} model")
     print("=" * 80)
     client = MlflowClient()
-    [pprint.pprint(dict(mv), indent=4) for mv in client.search_model_versions("name='WeatherForecastModel'")]
+    [pprint.pprint(dict(mv), indent=4) for mv in client.search_registered_models("name='IrisClassifierModel'")]
 
-    # get the maximum version
-    model_versions = client.search_registered_models("name='WeatherForecastModel'")
-    max_model_version = max([model_version.version for model_version in model_versions])
-    print(f"max_version = {max_model_version}")
-    print(f"max_version type is = {type(max_model_version)}")
