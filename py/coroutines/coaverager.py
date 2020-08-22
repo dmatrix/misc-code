2dfb995bd8df188bc75aba11c070780ffebd14df
Jules Damji <dmatrix@comcast.net>
2020-08-22 10:52:26 -0700

    Add example on insertions using insect

---
+++ py/coroutines/coaverager.py
@@ -0,0 +1,24 @@
+def averager():
+    total = 0.0
+    count = 0
+    average = None
+    # Infinite room until .close() is sent
+    while True:
+        # Suspend the coroutine and produce the result to the caller
+        term = yield average
+        total += term
+        count += 1
+        average = total/count
+
+if __name__ == "__main__":
+
+    # Create a coroutine object
+    coro_avg = averager()
+    next(coro_avg)
+    print(coro_avg.send(10))
+    print(coro_avg.send(5))
+    print(coro_avg.send(5))
+
+    # Terminate the coroutine
+    # coro_avg.close()
+
