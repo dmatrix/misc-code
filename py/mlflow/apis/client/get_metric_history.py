53fcb5d999db95c6d907536de49e9737ec255118
Jules Damji <dmatrix@comcast.net>
2020-10-22 10:39:18 -0700

    Adde step argument to log_metric method in get_metric_history()
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

--- py/mlflow/apis/client/get_metric_history.py
+++ py/mlflow/apis/client/get_metric_history.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     # Log couple of metrics, update their initial value, and fetch each
     # logged metrics' history.
     for k, v in [("m1", 1.5), ("m2", 2.5)]:
-        client.log_metric(run.info.run_id, k, v)
-        client.log_metric(run.info.run_id, k, v+1)
+        client.log_metric(run.info.run_id, k, v, step=0)
+        client.log_metric(run.info.run_id, k, v + 1, step=1)
         print_metric_info(client.get_metric_history(run.info.run_id, k))
     client.set_terminated(run.info.run_id)
