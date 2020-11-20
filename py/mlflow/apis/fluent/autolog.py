88c6e18157acd6e2d4abe8429daab786c817d73d
Jules Damji <dmatrix@comcast.net>
2020-11-20 09:50:38 -0800

    Added autolog snippet
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

---
+++ py/mlflow/apis/fluent/autolog.py
@@ -0,0 +1,33 @@
+import warnings
+
+import numpy as np
+import mlflow.sklearn
+from mlflow.tracking import MlflowClient
+from sklearn.linear_model import LinearRegression
+
+if __name__ == "__main__":
+
+    warnings.filterwarnings("ignore")
+    print(mlflow.__version__)
+
+    def print_autologged_info(r):
+        tags = {k: v for k, v in r.data.tags.items() if not k.startswith("mlflow.")}
+        artifacts = [f.path for f in MlflowClient().list_artifacts(r.info.run_id, "model")]
+        print("run_id: {}".format(r.info.run_id))
+        print("params: {}".format(r.data.params))
+        print("metrics: {}".format(r.data.metrics))
+        print("tags: {}".format(tags))
+        print("artifacts: {}".format(artifacts))
+
+    # prepare training data
+    X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
+    y = np.dot(X, np.array([1, 2])) + 3
+
+    # Auto log all the parameters, metrics, and artifacts
+    mlflow.autolog()
+    model = LinearRegression()
+    with mlflow.start_run() as run:
+        model.fit(X, y)
+
+    # fetch the auto logged parameters and metrics
+    print_autologged_info(run)
--- py/mlflow/apis/fluent/register_model.py
+++ py/mlflow/apis/fluent/register_model.py
@@ -7,15 +7,15 @@ from sklearn.ensemble import RandomForestRegressor
 
 if __name__ == "__main__":
 
-    mlflow.set_tracking_uri("sqlite:////tmp/mlruns.db")
+    mlflow.set_tracking_uri("sqlite:///mlruns.db")
     params = {"n_estimators": 3, "random_state": 42}
-
     # Log MLflow entities
     with mlflow.start_run() as run:
-        rfr = RandomForestRegressor(params)
+        rfr = RandomForestRegressor(**params).fit([[0, 1]], [1])
         mlflow.log_params(params)
         mlflow.sklearn.log_model(rfr, artifact_path="sklearn-model")
 
+    # Register the model
     model_uri = "runs:/{}/sklearn-model".format(run.info.run_id)
     mv = mlflow.register_model(model_uri, "RandomForestRegressionModel")
     print("Name: {}".format(mv.name))
