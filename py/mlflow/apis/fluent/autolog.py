cea7b94f01627b30f22f6687fb50e968e3e5cf8f
Jules Damji <dmatrix@comcast.net>
2020-11-24 14:16:56 -0800

    reverte back to not using pprint
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

--- py/mlflow/apis/fluent/autolog.py
+++ py/mlflow/apis/fluent/autolog.py
@@ -14,10 +14,10 @@ if __name__ == "__main__":
         tags = {k: v for k, v in r.data.tags.items() if not k.startswith("mlflow.")}
         artifacts = [f.path for f in MlflowClient().list_artifacts(r.info.run_id, "model")]
         print("run_id: {}".format(r.info.run_id))
+        print("artifacts: {}".format(artifacts))
         print("params: {}".format(r.data.params))
         print("metrics: {}".format(r.data.metrics))
         print("tags: {}".format(tags))
-        print("artifacts: {}".format(artifacts))
 
     # prepare training data
     X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
