6e70d9ba6394c8804c3ff8f86d16418e6d5af3d9
Jules Damji <dmatrix@comcast.net>
2020-12-11 17:21:14 -0800

    Clearified case 2 TS creates an instance of SQLAlchemyStore and logs the entities
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

--- py/mlflow/server/4_stores_scenario.py
+++ py/mlflow/server/4_stores_scenario.py
@@ -16,7 +16,8 @@ part 2: Tracking Server --> REST Response Response with artifact store URI --> M
 part 3: MLflowClient --> instance of [scheme]ArtifactStore --> S3/ftp/wasb/gc etc(store artifacts)
 
 MLflow Entities:
-part 1: MLflow Client --> instance of SQLAlchemyStore (for MLflow entities, params, runs, metrics, etc)
+part 1: MLflow Client APIs --> RestStore --> REST Request API Call --> Tracking Server --> instance of SQLAlchemyStore 
+(for MLflow entities, params, runs, metrics, etc) and write to sqlite file
 
 This will use the LocalArtifactFileStore (file:/tmp/my_artifacts) for saving artifacts and 
 backend SQLAlchemyStore (sqlite:///my_mlruns.db) for saving MLflow entities (runs, params, metrics, tags, etc).
@@ -34,7 +35,8 @@ part 2: Tracking Server --> REST Response Response with artifact store URI --> M
 part 3: MLflowClient --> instance of S3ArtifactStore --> S3(store artifacts)
 
 MLflow Entities:
-part 1: MLflow Client APIs --> RestStore --> REST Request API Call --> Tracking Server --> instance of SQLAlchemyStore (for MLflow entities, params, runs, metrics, etc)
+part 1: MLflow Client APIs --> RestStore --> REST Request API Call --> Tracking Server --> instance of SQLAlchemyStore 
+(for MLflow entities, params, runs, metrics, etc) writes to Postgres DB
 
 This will use the S3ArtifactFileStore (s3:/bucket_name/) for saving artifacts and 
 backend SQLAlchemyStore (postresql://URI) for saving MLflow entities (runs, params, metrics, tags, etc).
