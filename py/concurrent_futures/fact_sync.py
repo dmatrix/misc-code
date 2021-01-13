4251969ca4e96bfff4c473c3bcb220ef78fd2cc2
Jules Damji <dmatrix@comcast.net>
2021-01-13 09:25:00 -0800

    add more examples demonstrating sync vs. async
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

---
+++ py/concurrent_futures/fact_sync.py
@@ -0,0 +1,58 @@
+import asyncio
+import time
+from datetime import datetime
+
+
+async def custom_sleep(n):
+    """
+    Custom sleep that uses synchronous sleep
+
+    Parameters
+    ----------
+    n : int
+        Sleep for n seconds
+    """
+    print('SLEEP', datetime.now())
+    time.sleep(n)
+
+
+async def factorial(name, number):
+    """
+    A courtine to compute a factorial
+
+    Parameters
+    ----------
+    name: str
+        A label to indicate which coroutine is currently running
+    """
+
+    f = 1
+    for i in range(2, number + 1):
+        print('Task: {}: Compute factorial for: {}'.format(name, i))
+        # invoke custom sleep to yield to the next task
+        await custom_sleep(2)
+        f *= i
+    # Print the result after having awakened after sleep
+    print('Task {}: factorial({}) is {}\n'.format(name, number, f))
+
+
+if __name__ == '__main__':
+
+    # Main event loop here that queues the tasks
+    start = time.time()
+    loop = asyncio.get_event_loop()
+
+    # Create a queue of tasks
+    tasks = [
+        asyncio.ensure_future(factorial("A", 3)),
+        asyncio.ensure_future(factorial("B", 4))
+    ]
+
+    # loop until all tasks within the coroutines have been
+    # scheduled and completed
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
+
+    end = time.time()
+    print("Total time: {}".format(end - start))
+
