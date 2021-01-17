f36249b3a205e607e2ff70b9672562d1f97992a9
Jules Damji <dmatrix@comcast.net>
2021-01-17 14:49:20 -0800

    Use ABC
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

---
+++ py/pyobjects/shapes3.py
@@ -0,0 +1,57 @@
+from abc import ABCMeta, abstractmethod
+import logging
+
+
+class Shape(metaclass=ABCMeta):
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
+        self._logger.setLevel(logging.INFO)
+
+    @abstractmethod
+    def area(self, **kwargs):
+        pass
+
+
+class Triangle(Shape):
+    def __init__(self, *, base=0, height=0):
+        super(Triangle, self).__init__()
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return (self.base * self.height) / 2
+
+
+class Square(Shape):
+    def __init__(self, *, side=0):
+        super(Square, self).__init__()
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+
+class Rectangle(Shape):
+    def __init__(self, *, length=0, height=0):
+        super(Rectangle, self).__init__()
+        self.length = length
+        self.height = height
+
+    def area(self):
+        return self.length * self.height
+
+
+if __name__ == '__main__':
+    a = Triangle(height=2, base=12, )  # triangle
+    b = Square(side=4)  # square
+    c = Rectangle(height=5, length=4)  # rectangle
+    print(str(a.__class__))
+    print(a.area())
+    print(str(b.__class__))
+    print(b.area())
+    print(str(c.__class__))
+    print(c.area())
+    print(__name__)
+
+
+
