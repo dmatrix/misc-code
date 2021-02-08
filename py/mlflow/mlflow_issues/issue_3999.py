2ecd0e5c96960357c67911a8ee99a9c67709779a
Jules Damji <dmatrix@comcast.net>
2021-02-08 13:23:08 -0800

    try to update previously logged parameter
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

---
+++ py/mlflow/mlflow_issues/issue_3999.py
@@ -0,0 +1,25 @@
+import mlflow
+from mlflow.tracking import MlflowClient
+
+
+def print_run_info(r):
+    print("run_id: {}".format(r.info.run_id))
+    print("lifecycle_stage: {}".format(r.info.lifecycle_stage))
+    print("params: {}".format(r.data.params))
+
+
+if __name__ == '__main__':
+
+    with mlflow.start_run() as run:
+        mlflow.log_param("m1", 1)
+
+    # This ends this run
+    run = mlflow.get_run(run.info.run_id)
+    print_run_info(run)
+
+
+    # Now update the ended run using the CRUD MLFlowClient APIs
+    client = MlflowClient()
+    client.log_param(run.info.run_id, "m1", 2)
+    print("--" * 2)
+    print_run_info(run)
