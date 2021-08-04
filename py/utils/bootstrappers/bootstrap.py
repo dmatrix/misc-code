09488ceb5e6cbdab69e161af3dacd1737bab9ee9
Jules Damji <dmatrix@comcast.net>
2021-08-04 09:49:36 -0700

    modified to include config.yaml
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

--- py/utils/bootstrappers/bootstrap.py
+++ py/utils/bootstrappers/bootstrap.py
@@ -3,12 +3,20 @@ def bootstrap():
     import os
 
     path = Path(os.getcwd())
-    file_path = path / "config.yaml"
+    file_path = path / "driver.yaml"
+    config_path = path/ "config.yaml"
     driver_entities = [1001, 1002, 1003, 1004, 1005]
+    configs = {"path": os.getcwd(),
+              "provider": "local",
+              "online": True
+              }
     with open(file_path, "w") as f:
         for driver in driver_entities:
             f.write(f"driver: {str(driver)}")
             f.write("\n")
+    with open(config_path, "w") as f:
+        for k, v in configs.items():
+            f.write("".join([str(k),":",str(v),"\n"]))
 
 
 if __name__ == '__main__':
