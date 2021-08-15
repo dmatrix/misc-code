85529adc7c04a10eaa736173a789cbb06633ac11
Jules Damji <dmatrix@comcast.net>
2021-08-14 19:24:05 -0700

    Added getting started ray tutorial
    Signed-off-by: Jules Damji <dmatrix@comcast.net>

---
+++ ray/README.md
@@ -0,0 +1,35 @@
+
+![](images/ray_header_logo.png)
+# An Introduction to Ray: Getting Started with Concepts and Programming Patterns
+
+A hands-on tutorial about using [Ray](https://docs.ray.io/)
+core features and API to implement distributed Python applications.
+
+## Getting Started
+
+
+Then use `pip` to install the required dependencies:
+```
+python3 -m pip install -U pip
+python3 -m pip install -r requirements.txt
+python -m ipykernel install
+```
+
+If you use `conda` for installing Python packages:
+```
+conda create -n ray python=3.9
+conda activate ray
+python3 -m pip install -r requirements.txt
+conda install ipykernel --name Python3
+```
+
+jupyter-lab
+
+		
+## Other Recommended Reading
+
+  * [*Ray Design Patterns*](https://docs.google.com/document/d/167rnnDFIVRhHhK4mznEIemOtj63IOhtIPvSYaPgI4Fg/edit#heading=h.crt5flperkq3)
+  * ["Futures and Promises"](http://dist-prog-book.com/chapter/2/futures.html)
+  * ["Ray Distributed Library Patterns"](https://www.anyscale.com/blog/ray-distributed-library-patterns)
+  * [Anyscale Academy](https://github.com/anyscale/academy)
+  * [Ray Tutorial](https://github.com/ray-project/tutorial)
---
+++ ray/ray_tasks.py
@@ -0,0 +1,48 @@
+import os
+import time
+import multiprocessing
+import math
+import ray
+
+START_POINTS = [1, 100, 1000, 10000]
+END_POINTS = [1000, 10000, 100000, 1000000]
+
+
+def none_ray_task(a: int, b: int) -> int:
+    return math.sqrt(sum(x * x for x in range(a, b)))
+
+
+def print_pairs(a: int, b: int) -> None:
+    print((a, b))
+
+
+@ray.remote
+def ray_task(a: int, b: int) -> int:
+    print("pid={}; ppid={}".format(os.getpid(), os.getppid()))
+    return math.sqrt(sum(x * x for x in range(a, b)))
+
+
+if __name__ == '__main__':
+
+    n_cores = multiprocessing.cpu_count()
+    print("Number of cores: {}".format(n_cores))
+
+    # invoke local task
+    start = time.time()
+    for _ in range(n_cores):
+        print("Local return valued: {:.2f}". format(none_ray_task(1, 10000)))
+    print("Time elapsed for non ray task: {:.2f}".format(time.time() - start))
+
+    # Initialize on the local host
+    ray.init(num_cpus=n_cores)
+    start = time.time()
+    for _ in range(n_cores):
+        print("Remote Ray task returned value: {:.2f}".format(ray.get(ray_task.remote(1, 10000))))
+    print("Time elapsed for non Ray task: {:.2f}".format(time.time() - start))
+
+    [print_pairs(s, e) for s in START_POINTS for e in END_POINTS]
+
+    time.sleep(10000)
+
+
+
